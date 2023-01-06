eeb3e003f0b0f510d852d6c9c87dcc670be7fd3b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-01-06 20:46:52 +0100

    Navigate through signals and events

--- src/lib/facets/FacetExtractor.ts
+++ src/lib/facets/FacetExtractor.ts
@@ -1,4 +1,4 @@
-import { CatchEvent, Process, ThrowEvent } from 'bpmn-moddle';
+import { CatchEvent, Message, Process, Signal, ThrowEvent } from 'bpmn-moddle';
 import { PathLike } from 'fs';
 import { Model } from '../bpmn';
 import { AssignedValue } from './AssignedValue';
@@ -18,9 +18,61 @@ export class FacetExtractor {
 
   extract(models: Model | Model[]): PopulatedFacet[] {
     return this._classification.facets.map(facet => {
+      const signals: AssignedValue[] = this.extractSignals(facet, models);
+      const messages: AssignedValue[] = this.extractMessages(facet, models);
       const values = facet.values.map(value => this.assign(models, value, facet));
-      return { id: facet.id, name: facet.name, values };
+      return { id: facet.id, name: facet.name, values: [...signals, ...messages, ...values] };
     });
+  }
+
+  private extractSignals(facet: Facet, models: Model | Model[]) {
+    const signals: AssignedValue[] = [];
+    if (facet.id === "bpmn:Signal") {
+      ((models instanceof Array) ? models : [models])
+        .map(model => {
+          const elements: any[] = model.definitions.rootElement.rootElements;
+          const candidates: Signal[] = elements.filter(element => element.$type === 'bpmn:Signal');
+
+          candidates.forEach(candidate => {
+            const existing = signals.find(signal => signal.id === candidate.name);
+            if (existing) {
+              existing.locations.push(model.location);
+            } else {
+              signals.push({
+                id: candidate.name,
+                name: candidate.name,
+                locations: [model.location]
+              });
+            }
+          });
+        });
+    }
+    return signals;
+  }
+
+  private extractMessages(facet: Facet, models: Model | Model[]) {
+    const messages: AssignedValue[] = [];
+    if (facet.id === "bpmn:Message") {
+      ((models instanceof Array) ? models : [models])
+        .map(model => {
+          const elements: any[] = model.definitions.rootElement.rootElements;
+          const candidates: Message[] = elements.filter(element => element.$type === 'bpmn:Message');
+
+          candidates.forEach(candidate => {
+            const existing = messages.find(message => message.id === candidate.name);
+            if (existing) {
+              existing.locations.push(model.location);
+            } else {
+              messages.push({
+                id: candidate.name,
+                name: candidate.name,
+                locations: [model.location]
+              });
+            }
+          });
+        });
+    }
+    return messages;
   }
 
   private assign(models: Model | Model[], value: Value, facet: Facet): AssignedValue {
