97f304b441c390a5d3760df8ebf8002a2fb5585c
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-01-06 19:26:38 +0100

    Prompt for facets to be selected

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
-import { EnrichmentService } from '../lib/enrichment';
 import { ApplicationProperties } from '../lib/config';
+import { EnrichmentService } from '../lib/enrichment';
 import { FacetExtractor } from '../lib/facets';
+import { InteractiveFacetSelector } from '../lib/filter';
 import { ProcessImporter } from '../lib/import';
 
 export class Application {
@@ -9,6 +10,7 @@ export class Application {
   private static _enrichmentService: EnrichmentService;
   private static _importer: ProcessImporter;
   private static _facetExtractor: FacetExtractor;
+  private static _selector: InteractiveFacetSelector;
 
   static async run(properties: ApplicationProperties) {
     console.log('started w/ properties', properties);
@@ -17,8 +19,10 @@ export class Application {
     Application._enrichmentService = new EnrichmentService()
     Application._importer = new ProcessImporter(this._properties, this._enrichmentService);
     Application._facetExtractor = new FacetExtractor();
+    Application._selector = new InteractiveFacetSelector()
 
     const models = await this._importer.import();
     const facets = this._facetExtractor.extract(models);
+    const selected = await this._selector.filter(facets);
   }
 }
---
+++ src/lib/filter/InteractiveFacetSelector.ts
@@ -0,0 +1,35 @@
+import inquirer from 'inquirer';
+import { PopulatedFacet } from '../facets';
+
+export class InteractiveFacetSelector {
+
+  async filter(facets: PopulatedFacet[]): Promise<PopulatedFacet[]> {
+    const choices = facets
+      .flatMap(facet => {
+        const clazz = new inquirer.Separator(facet.name);
+        const options = facet.values
+          .filter(value => value.locations.length != 0)
+          .map(val => {
+            return { name: val.name, value: `${facet.id}.${val.id}` }
+          })
+        return [clazz, ...options]
+      });
+
+    const answer = await inquirer.prompt([{
+      type: 'checkbox',
+      message: 'Facettennavigation',
+      name: 'result',
+      choices,
+    }]);
+
+    return answer.result.flatMap((property: string) => {
+      const [facetId, valueId] = property.split('.');
+      return facets
+        .filter(facet => facet.id === facetId)
+        .flatMap(facet => {
+          const selected = facet.values.filter(value => value.id === valueId);
+          return { ...facet, values: selected }
+        })
+    });
+  }
+}
