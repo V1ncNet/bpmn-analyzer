350bc0cd7048d188f80acb1381a0648ec0ecc2c7
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-01-06 16:49:40 +0100

    Assign properties

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,7 +2,11 @@ import { ApplicationProperties } from '../lib/config';
 
 export class Application {
 
+  private static _properties: ApplicationProperties;
+
   static async run(properties: ApplicationProperties) {
     console.log('started w/ properties', properties);
+
+    Application._properties = properties;
   }
 }
