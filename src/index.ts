8b327b792fbbc87ede2ba16fbd9b673e8681f0ed
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-01-07 10:30:40 +0100

    Extract application configuration

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { resolve } from 'path';
 import { Application } from './app';
-import { ApplicationProperties } from './lib/config';
+import { ConsoleConfigurator } from './lib/config/ConsoleConfigurator';
 
 async function main() {
-  const program = new Command();
-
-  program
-    .name('bpmn-analyzer')
-    .description('CLI zur Analyse von Beziehungen und Abhängigkeiten zwischen BPMN-Modellen');
-
-  program
-    .option('-p, --path <pfad>', 'Speicherort der BPMN-Modellen', './')
-    .option('-r, --recursive', 'Konfiguriert, ob [pfad] rekursiv durchsucht werden soll');
-
-  program.parse();
-
-  const options = program.opts();
-  const basePath = resolve(options['path']);
-
-  const properties: ApplicationProperties = {
-    basePath,
-    recursive: options['recursive'] || false,
-  };
-
-  await Application.run(properties);
+  ConsoleConfigurator.run();
+  await Application.run(ConsoleConfigurator.properties);
 }
 
 main();
---
+++ src/lib/config/ConsoleConfigurator.ts
@@ -0,0 +1,30 @@
+import { Command } from 'commander';
+import { resolve } from 'path';
+import { ApplicationProperties } from './ApplicationProperties';
+
+export class ConsoleConfigurator {
+
+  public static properties: ApplicationProperties;
+
+  public static run() {
+    const program = new Command();
+
+    program
+      .name('bpmn-analyzer')
+      .description('CLI zur Analyse von Beziehungen und Abhängigkeiten zwischen BPMN-Modellen');
+
+    program
+      .option('-p, --path <pfad>', 'Speicherort der BPMN-Modellen', './')
+      .option('-r, --recursive', 'Konfiguriert, ob [pfad] rekursiv durchsucht werden soll');
+
+    program.parse();
+
+    const options = program.opts();
+    const basePath = resolve(options['path']);
+
+    ConsoleConfigurator.properties = {
+      basePath,
+      recursive: options['recursive'] || false,
+    };
+  }
+}
