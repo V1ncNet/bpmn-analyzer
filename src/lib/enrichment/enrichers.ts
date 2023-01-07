74d3189ea3837c929ee04e4037a9c3d55b2f5481
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-01-07 14:46:31 +0100

    Extract methods

--- src/lib/enrichment/enrichers.ts
+++ src/lib/enrichment/enrichers.ts
@@ -1,4 +1,4 @@
-import { Process } from 'bpmn-moddle';
+import { Process, RootElement } from 'bpmn-moddle';
 import { lstat } from 'fs/promises';
 import { Model } from '../bpmn';
 import { InformationEnricher } from './InformationEnricher';
@@ -9,14 +9,24 @@ export const MtimeEnricher: InformationEnricher = async (model: Model) => {
 }
 
 export const ComplexityEnricher: InformationEnricher = async (model: Model) => {
-  const elements: any[] = model.definitions.rootElement.rootElements;
-  const process: Process = elements.find(element => element.$type === 'bpmn:Process');
+  const elements = rootElements(model);
+  const process = findProcessIn(elements);
   const flowElements = process.flowElements;
+
   model.properties['complexity'] = flowElements.length;
 }
 
 export const ProcessIdEnricher: InformationEnricher = async (model: Model) => {
-  const elements: any[] = model.definitions.rootElement.rootElements;
-  const process: Process = elements.find(element => element.$type === 'bpmn:Process');
+  const elements = rootElements(model);
+  const process = findProcessIn(elements);
+
   model.properties['processId'] = process.id;
+}
+
+function rootElements(model: Model): any[] {
+  return model.definitions.rootElement.rootElements;
+}
+
+function findProcessIn(elements: RootElement[]): Process {
+  return elements.find(element => element.$type === 'bpmn:Process') as Process;
 }
